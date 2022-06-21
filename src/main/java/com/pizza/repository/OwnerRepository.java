94c016ec4b359eb62a2a139b57fde765ece308db
George Niculae <george-cosmin.niculae@endava.com>
2022-06-21 12:18:36 +0300

    Updating OwnerRepository

--- src/main/java/com/pizza/repository/OwnerRepository.java
+++ src/main/java/com/pizza/repository/OwnerRepository.java
@@ -4,8 +4,12 @@ import com.pizza.entity.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
+
+    Optional<Owner> findByFirstNameAndLastName(String firstName, String lastName);
 
     void deleteByFirstNameAndLastName(String firstName, String lastName);
 }
