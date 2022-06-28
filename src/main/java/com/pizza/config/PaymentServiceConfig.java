8cabed18d2927327d155a45974562bc60a095679
George Niculae <george-cosmin.niculae@endava.com>
2022-06-28 10:54:44 +0300

    Updating PaymentServiceConfig

--- src/main/java/com/pizza/config/PaymentServiceConfig.java
+++ src/main/java/com/pizza/config/PaymentServiceConfig.java
@@ -12,6 +12,5 @@ import org.springframework.context.annotation.Configuration;
 public class PaymentServiceConfig {
 
     private String baseUrl;
-    private String addPaymentUrl;
     private String updatePaymentUrl;
 }
